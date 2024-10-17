0e9a15eeb6a882d4d23f172ea04638bd4248e415
Vidyesh Churi <vidyesh95@gmail.com>
2024-10-17 16:55:50 +0530

    added base urls

--- laniakea_jewelry/urls.py
+++ laniakea_jewelry/urls.py
@@ -22,5 +22,7 @@ from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # path("", include("app.urls")),
+    # path("social-auth", include("social_django.urls", namespace="social")),
 ]
 # ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
